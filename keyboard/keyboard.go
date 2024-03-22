7aa11f5eeb20cefb1ac3b05f57974ae6f2e43c63
Stephen Gao <stephen.gao@calix.com>
2024-03-22 09:29:10 +0800

    keyboard

---
+++ keyboard/keyboard.go
@@ -0,0 +1,22 @@
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return grade, err
+}
