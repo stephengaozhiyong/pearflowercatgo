b32bb0596ab5bd230c324f2e7b9ff6bf13409b03
Stephen Gao <stephen.gao@calix.com>
2024-03-22 09:17:25 +0800

    miss

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -3,9 +3,9 @@ package greeting
 import "fmt"
 
 func Hello() {
-	fmt.Println("Hello DongYin. I love you.")
+	fmt.Println("Hello DongYin. I miss you.")
 }
 
 func Hi() {
-	fmt.Println("Hi DongYin. I lvoe you.")
+	fmt.Println("Hi DongYin. I miss you.")
 }
