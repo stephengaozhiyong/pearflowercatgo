df47c289fd858845e7f408d578de14f4e1db5037
root <root@DESKTOP-ODKQK3P.localdomain>
2024-03-23 10:06:02 +0800

    change greeting/greeting.go

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -1,11 +1,14 @@
+//这个包是用来问候董颖的
 package greeting
 
 import "fmt"
 
+//我向董颖说hello
 func Hello() {
-	fmt.Println("Hello DongYin. I hate you.")
+	fmt.Println("Hello DongYin. I love you.")
 }
 
+//我向董颖说hi
 func Hi() {
-	fmt.Println("Hi DongYin. I hate you.")
+	fmt.Println("Hi DongYin. I love you.")
 }
